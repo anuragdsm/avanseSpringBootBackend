796b2f10d2707de359c871ac9ec613fcb8319e62
Anurag Kumar <anurag@digistreetmedia.in>
2021-12-13 12:25:33 +0530

    live reload implemented... Logout functionality implemented via layout.

---
+++ src/main/java/com/avanse/springboot/model/PostCategory.java
@@ -0,0 +1,36 @@
+package com.avanse.springboot.model;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="POST_CATEGORIES")
+public class PostCategory implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	public PostCategory() {
+		super();
+
+	}
+	
+	public PostCategory(String name) {
+		super();
+		this.name = name;
+	}
+}
+
+
